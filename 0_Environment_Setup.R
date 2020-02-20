6f24ae006a01d0e2ea3bb1afbab38e3d1892f997
lisancao <lisanatashacao@gmail.com>
2020-02-19 18:29:19 -0800

    update basic commands

--- 0_Environment_Setup.R
+++ 0_Environment_Setup.R
@@ -4,8 +4,15 @@ setwd() #set
 
 #get help on a command 
 help()
+?
+args()
 
 #list objects in environment in a vector of strings 
 ls() 
 
-#
+#save a image (file w/ env obj)
+save.image(file = "" )
+save() #same thing 
+
+#remove objects
+rm() 
