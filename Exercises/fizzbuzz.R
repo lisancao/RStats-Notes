9184606d1d1e67ebe4490823b141b8baea15c03d
lisancao <lisanatashacao@gmail.com>
2020-02-26 00:08:30 -0800

    added exercises folder, rename mortality and province stats to dataplay1 and dataplay2 respectively

---
+++ Exercises/fizzbuzz.R
@@ -0,0 +1,2 @@
+#Write a program that prints the numbers from 1 to 100, but for multiples of three print "fuzz" instead of the number and for multiples of five print "Buzz". For numbers that are multiples of both three and five print "FizzBuzz" 
+
---
+++ Exercises/seashells.R
@@ -0,0 +1,11 @@
+#This is the text for the famous “sea shells” tongue twister:
+  sea_shells <- c(
+    "She", "sells", "sea", "shells", "by", "the", "seashore",
+    "The", "shells", "she", "sells", "are", "surely", "seashells",
+    "So", "if", "she", "sells", "shells", "on", "the", "seashore",
+    "I'm", "sure", "she", "sells", "seashore", "shells"
+  )
+ # Use the nchar function to calculate the number of letters in each word. Now loop
+#  over possible word lengths, displaying a message about which words have that
+ # length. For example, at length six, you should state that the words “shells” and
+#  “surely” have six letters. 
